9d537c876f29d2d0814dfcf50c04cabe860339af
Lizter <lizterdsouza@gmail.com>
2022-12-30 23:54:59 +0530

    code1

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,10 +1,24 @@
 package com.driver;
 
-public class CurrentAccount extends BankAccount{
+import java.security.Key;
+import java.io.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+import static com.driver.isValid.rearrangeString;
+
+public class CurrentAccount extends BankAccount {
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name, balance, 5000);
+        if (balance < getMinBalance()) {
+            throw new Exception("Insufficient Balance");
+        }
+        this.tradeLicenseId = tradeLicenseId;
 
     }
 
@@ -14,6 +28,22 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
-    }
+        boolean valid = true;
+        for (int i = 0; i < tradeLicenseId.length() - 1; i++) {
+            if (tradeLicenseId.charAt(i) == tradeLicenseId.charAt(i + 1)) {
+                valid = false;
+                break;
+            }
+        }
 
+        if(valid == false) {
+            String str=tradeLicenseId;
+            String res = rearrangeString(str);
+            if(res =="")
+                throw new Exception("Valid License can not be generated");
+            else
+                this.tradeLicenseId = res;
+        }
+    }
 }
+
