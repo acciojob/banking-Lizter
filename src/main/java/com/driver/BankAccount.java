0cdb528eb99735a814b0ff95b78b680bf8ada43a
Lizter <lizterdsouza@gmail.com>
2022-12-31 14:22:56 +0530

    codef1

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,4 +1,5 @@
 package com.driver;
+import java.util.Random;
 
 public class BankAccount {
 
@@ -41,58 +42,61 @@ public class BankAccount {
 
     private String accNo;
 
-    public String generateAccountNumber(int digits, int sum) throws Exception{
+    public String generateAccountNumber(int digits, int sum) throws Exception {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
 
-        accNo="";
+        accNo = "";
 
-        //generate("",digits,sum);
-        if(accNo.length()==0){
+        generate(accNo,digits,sum);
+        if (accNo.length() == 0) {
             throw new Exception("Account Number can not be generated");
         }
         return accNo;
     }
 
 
-
     public void deposit(double amount) {
         //add amount to balance
-        balance=balance+amount;
+        balance = balance + amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
 
-        if(balance-amount>=minBalance){
-            balance=balance-amount;
-        }
-        else {
+        if (balance - amount >= minBalance) {
+            balance = balance - amount;
+        } else {
             throw new Exception("Insufficient Balance");
         }
 
 
     }
 
-    /* public void generate(String gAccNo, int digits, int sum){   //me
 
-        if(gAccNo.length()>=digits){
-            int cSum=0;
-            for(int i=0;i<gAccNo.length();i++) {
-                cSum += (int)(gAccNo.charAt(i)-'0');
+    public void generate(String gAccNo, int digits, int sum) {   //me
+
+        if (gAccNo.length() > digits) {
+            return;
+        }
+        if (gAccNo.length() == digits) {
+            int cSum = 0;
+            for (int i = 0; i < gAccNo.length(); i++) {
+                cSum += (int) (gAccNo.charAt(i) - '0');
             }
-            if(cSum==sum){
-                this.accNo=gAccNo;
+            if (cSum == sum) {
+                this.accNo = gAccNo;
             }
             return;
         }
 
-        for(int i=0;i<10;i++){
-            generate(gAccNo+Integer.toString(i), digits, sum);
+        for (int i = 0; i < 10; i++) {
+            generate(gAccNo + Integer.toString(i), digits, sum);
         }
 
-     */
+
     }
+}
 
